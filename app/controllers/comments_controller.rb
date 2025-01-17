9a5aacfe09efda79d8cc12868255a7b232656323
kojiro-hara <jirou884989@gmail.com>
2025-01-17 13:20:11 +0900

    コメント機能エラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype_id)
     else
       @prototype = @comment.prototype
-      @comment = @prototype.comments
+      @comments = @prototype.comments
       render "prototypes/show", status: :unprocessable_entity
     end
   end
